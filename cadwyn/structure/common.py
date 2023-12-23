781529588c7391f3cdca4d8b55451286e0935b14
Stanislav Zmiev <zmievsa@gmail.com>
2023-12-24 01:38:15 +0300

    Fix CI

--- cadwyn/structure/common.py
+++ cadwyn/structure/common.py
@@ -1,11 +1,11 @@
-import datetime
 from collections.abc import Callable
+from datetime import date
 from typing import ParamSpec, TypeAlias, TypeVar
 
 from pydantic import BaseModel
 
 VersionedModel = BaseModel
-VersionDate = datetime.date
+VersionDate = date
 _P = ParamSpec("_P")
 _R = TypeVar("_R")
 Endpoint: TypeAlias = Callable[_P, _R]
