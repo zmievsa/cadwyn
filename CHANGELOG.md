f301303c1a322781ced8b1241fc0020455921d7d
Stanislav Zmiev <zmievsa@gmail.com>
2024-02-07 16:14:23 +0400

    Fix wrong imports on latest aliases (#139)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ Please follow [the Keep a Changelog standard](https://keepachangelog.com/en/1.0.
 
 ## [Unreleased]
 
+## [3.4.2]
+
+### Fixed
+
+* Fix import aliases in nested `__init__.py` files generating incorrectly for latest version
+
 ## [3.4.1]
 
 ### Fixed
--- cadwyn/codegen/_plugins/latest_version_aliasing.py
+++ cadwyn/codegen/_plugins/latest_version_aliasing.py
@@ -64,8 +64,7 @@ class _ImportedModule:
         module = f"{self.path}.{self.name}"
         name = ast.alias(name="*")
         level = self.how_far_up_is_base_schema_dir_from_current_module
-        if self.is_package and level == 2:
-            level -= 1
+
         return ast.ImportFrom(
             level=level,
             module=module,
@@ -93,6 +92,7 @@ def _prepare_imports_from_version_dirs(
     how_far_up_is_base_schema_dir_from_current_module = (
         len(original_module_parts) - index_of_latest_package_dir_in_pythonpath
     )
+
     is_package = original_module_parts[-1] == "__init__"
     if is_package:
         original_module_parts.pop(-1)
