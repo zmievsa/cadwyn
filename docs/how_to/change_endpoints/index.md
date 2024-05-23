8a0b928f6ef83b808c6af3f6aeb5be44274a94fe
Stanislav Zmiev <zmievsa@gmail.com>
2024-05-23 10:13:31 +0400

    Extend dependency tests and docs

--- docs/how_to/change_endpoints/index.md
+++ docs/how_to/change_endpoints/index.md
@@ -8,3 +8,41 @@ It is not a breaking change so it's recommended to simply add it to all versions
 ## Delete an old endpoint
 
 See [concepts](../../concepts/endpoint_migrations.md#defining-endpoints-that-didnt-exist-in-new-versions)
+
+## Change an attribute of an endpoint
+
+Changing a "decoratory" attribute such as description or summary is generally not a breaking change and should just be applied to all versions. However, if such a change is required, you can just follow the instructions in the respective [concepts docs](../../concepts/endpoint_migrations.md#changing-endpoint-attributes).
+
+There are, however, attributes that can significantly affect our end user -- for example,
+
+This is not a breaking change in terms of requests but it [**can be**](#why-enum-expansion-is-a-breaking-change-for-responses) a breaking change in terms of responses.
+
+So if you do consider it a breaking change in terms of responses, you should do the following:
+
+1. Add `moderator` value into `data.head.users.BaseUserRoleEnum`
+2. Add the following migration to `versions.v2001_01_01`:
+
+    ```python
+    from cadwyn.structure import VersionChange, endpoint
+
+
+    class AddModeratorRoleToUser(VersionChange):
+        description = (
+            "Add 'moderator' role to users that represents an admin that "
+            "cannot create or remove other admins. This allows for a "
+            "finer-grained permission control."
+        )
+        instructions_to_migrate_to_previous_version = (
+            enum(UserRoleEnum).didnt_have("moderator"),
+        )
+
+        @convert_response_to_previous_version_for(UserResource)
+        def change_moderator_to_regular(response: ResponseInfo):
+            if response.body["role"] == "moderator":
+                response.body["role"] = "regular"
+    ```
+
+We convert moderators to regulars in older versions because it is a safer choice for our users.
+
+
+## Rename an endpoint
--- tests/test_router_generation.py
+++ tests/test_router_generation.py
@@ -105,8 +105,9 @@ class CreateVersionedAPIRoutes:
     def __call__(
         self,
         *version_changes: type[VersionChange],
+        router: VersionedAPIRouter | None = None,
     ) -> tuple[list[APIRoute], list[APIRoute]]:
-        app = self.create_versioned_app(*version_changes)
+        app = self.create_versioned_app(*version_changes, router=router)
         return (
             cast(list[APIRoute], app.router.versioned_routers.get(date(2000, 1, 1), APIRouter()).routes),
             cast(list[APIRoute], app.router.versioned_routers.get(date(2001, 1, 1), APIRouter()).routes),
@@ -306,6 +307,41 @@ def test__endpoint_had_dependencies(
     assert len(routes_2001[1].dependencies) == 1
 
 
+def test__endpoint_had_dependencies__with_extra_deps_added_at_router_level__router_deps_should_be_preserved(
+    create_versioned_clients: CreateVersionedClients,
+):
+    async def validation_buffer_dependency():
+        return []
+
+    async def add_to_validation_buffer_dependency_router_level(
+        validation_buffer: Annotated[list[str], Depends(validation_buffer_dependency)],
+    ):
+        validation_buffer.append("router")
+
+    async def add_to_validation_buffer_dependency_route_level(
+        validation_buffer: Annotated[list[str], Depends(validation_buffer_dependency)],
+    ):
+        validation_buffer.append("route")
+
+    router = VersionedAPIRouter(dependencies=[Depends(add_to_validation_buffer_dependency_router_level)])
+
+    @router.post("/hello")
+    def route(
+        validation_buffer: Annotated[list[str], Depends(validation_buffer_dependency)],
+    ):
+        return validation_buffer
+
+    client_2000, client_2001 = create_versioned_clients(
+        version_change(
+            endpoint("/hello", ["POST"]).had(dependencies=[Depends(add_to_validation_buffer_dependency_route_level)]),
+        ),
+        router=router,
+    ).values()
+
+    assert client_2000.post("/hello").json() == ["router", "route"]
+    assert client_2001.post("/hello").json() == ["router"]
+
+
 def test__only_exists_in_older_versions__endpoint_is_not_a_route__error(
     router: VersionedAPIRouter,
     test_endpoint: Endpoint,
@@ -326,7 +362,6 @@ def test__only_exists_in_older_versions__applied_twice__should_raise_error(
     router: VersionedAPIRouter,
     create_versioned_api_routes: CreateVersionedAPIRoutes,
 ):
-    # with insert_pytest_raises():
     with pytest.raises(
         CadwynError,
         match=re.escape('The route "test_endpoint" was already deleted. You can\'t delete it again.'),
@@ -355,6 +390,19 @@ def test__router_generation__changing_a_deleted_endpoint__error(
         ),
     ):
         create_versioned_app(version_change(endpoint("/test", ["GET"]).had(description="Hewwo")))
+
+
+def test__router_generation__changing_a_non_existent_endpoint__error(
+    router: VersionedAPIRouter,
+    create_versioned_app: CreateVersionedApp,
+):
+    with pytest.raises(
+        RouterGenerationError,
+        match=re.escape(
+            'Endpoint "[\'GET\'] /test" you tried to change in "MyVersionChange" doesn\'t exist',
+        ),
+    ):
+        create_versioned_app(version_change(endpoint("/test", ["GET"]).had(dependencies=[])))
 
 
 def test__router_generation__re_creating_an_existing_endpoint__error(
